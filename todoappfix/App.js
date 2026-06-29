72db9c03dc39e6303e73771112e205dbcfd420ad
tcombs64 <stephcombs78@icloud.com>
2026-06-29 12:52:59 -0400

    Add todoappfix: working Todo app on Expo SDK 54

---
+++ todoappfix/App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/task';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+     <View style={styles.tasksWrapper}>
+      <Text style={styles.sectionTitle}> Today's tasks</Text>
+
+
+
+      <View style={styles.items}>
+        {/* this is where the tasks will go*/}
+        <Task text={'Task 1'} />
+        <Task text={'Task 2'} />
+      </View>
+     </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {
+    marginTop:30,
+  },
+});
